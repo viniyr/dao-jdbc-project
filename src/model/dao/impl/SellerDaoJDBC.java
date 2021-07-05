3bf8a129358c9159f431673492a58fe16fe5bbfe
Vinicius Yonezawa <vinicius.yonezawa@gmail.com>
2021-07-05 19:47:50 -0300

    Procura geral

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -87,12 +87,46 @@ public class SellerDaoJDBC implements SellerDao{
 
 	@Override
 	public List<Seller> findAll() {
-		return null;
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		
+		try { 
+			st = conn.prepareStatement(
+					"SELECT seller.*,department.Name as DepName FROM seller "
+					+ "INNER JOIN department ON seller.DepartmentId = department.Id "
+					+ "ORDER BY Name"
+					);
+			
+			rs = st.executeQuery();
+			
+			List<Seller> list = new ArrayList<>();
+			Map<Integer, Department> map = new HashMap<>();
+			
+			while (rs.next()) { 
+				
+				Department dep = map.get(rs.getInt("DepartmentId"));	// O map serve para não criar uma nova instância de DEPARTMENT
+																		// e sim apontar para um já existente.
+				if (dep == null) { 
+					dep = instantiateDepartment(rs);
+					map.put(rs.getInt("DepartmentId"), dep);
+				}
+				
+				Seller obj = instantiateSeller(rs, dep);
+				list.add(obj);;
+			}
+			return list;
+		} 
+		catch (SQLException e) { 
+			throw new DbException(e.getMessage());
+		}
+		finally { 
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+		}
 	}
 
 	@Override
 	public List<Seller> findByDepartment(Department department) {
-		
 		PreparedStatement st = null;
 		ResultSet rs = null;
 		
@@ -113,8 +147,8 @@ public class SellerDaoJDBC implements SellerDao{
 			
 			while (rs.next()) { 
 				
-				Department dep = map.get(rs.getInt("DepartmentId"));	// O map serve para não criar uma nova instância de DEPARTMENT
-																		// e sim apontar para um já existente.
+				Department dep = map.get(rs.getInt("DepartmentId"));	
+																		
 				if (dep == null) { 
 					dep = instantiateDepartment(rs);
 					map.put(rs.getInt("DepartmentId"), dep);
