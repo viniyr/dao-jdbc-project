c59271ca4716befe28e8f25643a9629279d8b8b6
Vinicius Yonezawa <vinicius.yonezawa@gmail.com>
2021-07-05 20:38:34 -0300

    Inserir

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,7 @@
 package application;
 
 
+import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -30,6 +31,11 @@ public class Program {
 		for (Seller obj : list) { 
 			System.out.println(obj);
 		}
+		
+		System.out.println("\n--- TEST 2: seller INSERT---");
+		Seller newSeller = new Seller(null, "BMO", "imbmo@gmail.com", new Date(), 4000.0, department);
+		sellerDao.insert(newSeller);
+		System.out.println("Inserted! New id = " + newSeller.getId());
 	}
 
 }
