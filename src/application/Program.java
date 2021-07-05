5af3021e4e3ab2d2fbeb3d20d12f4ef63d5370df
Vinicius Yonezawa <vinicius.yonezawa@gmail.com>
2021-07-05 01:32:38 -0300

    Procurar por ID

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,18 +1,18 @@
 package application;
 
-import java.util.Date;
 
-import model.entities.Department;
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 
-		Department obj = new Department(1, "Books");
-		System.out.println(obj);
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
-		Seller seller = new Seller(21, "Vinicius", "vinicius.yonezawa@gmail.com", new Date(), 5000.00, obj);
+		Seller seller = sellerDao.findById(7);
+		
 		System.out.println(seller);
 	}
 
--- src/model/dao/SellerDao.java
+++ src/model/dao/SellerDao.java
@@ -9,7 +9,7 @@ public interface SellerDao {
 	void insert(Seller obj);
 	void update(Seller obj);
 	void deleteById(Integer id);
-	void findById(Integer id);
+	Seller findById(Integer id);
 	List<Seller> findAll();
 	
 }
