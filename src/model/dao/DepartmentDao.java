4ae5190824e34957dab5d2785b0dad36322e3835
Vinicius Yonezawa <vinicius.yonezawa@gmail.com>
2021-07-05 00:42:12 -0300

    Interfaces

---
+++ src/model/dao/DepartmentDao.java
@@ -0,0 +1,15 @@
+package model.dao;
+
+import java.util.List;
+
+import model.entities.Department;
+
+public interface DepartmentDao {
+
+	void insert(Department obj);
+	void update(Department obj);
+	void deleteById(Integer id);
+	void findById(Integer id);
+	List<Department> findAll();
+	
+}
